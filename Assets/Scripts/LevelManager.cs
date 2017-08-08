b168b485724f7bacec7f53e75eea0979955a55b4
Thiago de Bastos <thiago@thiagodebastos.com>
2017-08-08 22:41:45 +1000

    initial commit, menu ready

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        /*  - Caveats for Application.Quit()
+            - gs works great if building for PC and Console
+            - does not work on web build
+            - application should not quit itself on mobile
+        */
+        Application.Quit();
+    }
+}
