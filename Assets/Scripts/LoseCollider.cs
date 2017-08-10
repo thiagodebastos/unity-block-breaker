91c6199165c931fca4388c4a082a5c462d9a340d
Thiago de Bastos <thiago@thiagodebastos.com>
2017-08-10 22:21:11 +1000

    add Loose Collider script and initial lose screen

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    public LevelManager levelManager;
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        Debug.Log(trigger);
+        levelManager.LoadLevel("Lose Screen");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    static MusicPlayer instance = null;
+
+    void Awake()
+    {
+        Debug.Log("MusicPlayer Awake " + GetInstanceID());
+        // if an instance of MusicPlayer already exists, self-destruct
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            // claim the instance
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("MusicPlayer Start " + GetInstanceID());
+
+    }
+}
